396e49264df95f2dbb0578f7da5ac097af52305b
Ivan Gromenko <ivangromenko.1003@gmail.com>
2025-12-09 17:56:52 +0200

    project init

---
+++ app.ts
@@ -0,0 +1,9 @@
+import { createServer } from 'node:http';
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, TypeScript with Node.js!\n');
+}
+);
+
+server.listen(3000);
